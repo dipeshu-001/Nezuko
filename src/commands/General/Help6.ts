0dac2765a27544593337d1bb5572812f40c973ec
Toshiro <91844428+ToshiroHitsugaya223@users.noreply.github.com>
2022-01-21 15:43:39 +0530

    Update Help6.ts

--- src/commands/General/Help6.ts
+++ src/commands/General/Help6.ts
@@ -24,7 +24,7 @@ export default class Command extends BaseCommand {
         let well = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: well }, MessageType.video, {quoted:M.WAMessage,
             mimetype: Mimetype.gif,
-            caption: `-🄼🄾🄳🄴🅁🄰🅃🄸🄾🄽 🄻🄸🅂🅃 🧣
+            caption: `-ᴍᴏᴅʀᴀᴛɪᴏɴ ʟɪsᴛ
 ──────────────
 🧧 ${this.client.config.prefix}ᴀᴄᴛɪᴠᴀᴛᴇ
 🧧 ${this.client.config.prefix}ᴅᴇᴀᴄᴛɪᴠᴀᴛᴇ
