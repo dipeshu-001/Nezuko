b5d8d90dca4b5c0bc7b57fa53b650a1e1fee3fb7
Toshiro <91844428+ToshiroHitsugaya223@users.noreply.github.com>
2022-01-21 16:43:53 +0530

    Update Help8.ts

--- src/commands/General/Help8.ts
+++ src/commands/General/Help8.ts
@@ -24,7 +24,7 @@ export default class Command extends BaseCommand {
         let well = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: well }, MessageType.video, {quoted:M.WAMessage,
             mimetype: Mimetype.gif,
-            caption: `-🄽🅂🄵🅆 🄻🄸🅂🅃 🧣
+            caption: `-ɴsғᴡ ʟɪsᴛ
 ──────────────
 🍒 ${this.client.config.prefix}ᴀɴᴀʟ
 🍒 ${this.client.config.prefix}ʙʟᴏᴡᴊᴏʙ
