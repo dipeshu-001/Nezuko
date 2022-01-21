5f999e2d7ac799526aa0a64f5b13a4c37d40e3d0
Toshiro <91844428+ToshiroHitsugaya223@users.noreply.github.com>
2022-01-21 16:57:00 +0530

    Update Rin.ts

--- src/commands/Misc/Rin.ts
+++ src/commands/Misc/Rin.ts
@@ -19,10 +19,10 @@ export default class Command extends BaseCommand {
         const n = [
             './assets/Rin/rin-git.mp4'
         ]
-        let rin = n[Math.floor(Math.random() * n.length)]
+        let Nezuko = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: rin }, MessageType.video, {quoted:M.WAMessage,
             mimetype: Mimetype.gif,
-            caption: `🎗 Rin\n\n🎗️ *Description: Follow my Instagram* \n\n ⭐*URL-https://instagram.com/itz_toshiro12/* \n` }
+            caption: `🎗 Nezuko\n\n🎗️ *Description: Follow my Instagram* \n\n ⭐*URL-https://instagram.com/itz_toshiro12/* \n` }
         )
     }
 }
