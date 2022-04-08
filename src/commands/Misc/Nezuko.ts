71f5eb87cb8d46f3393764aaf63640e7893fd34c
Toshiro <91844428+Toshiro223@users.noreply.github.com>
2022-04-09 01:29:15 +0530

    Update Nezuko.ts

--- src/commands/Misc/Nezuko.ts
+++ src/commands/Misc/Nezuko.ts
@@ -9,17 +9,17 @@ import { ISimplifiedMessage } from "../../typings";
 export default class Command extends BaseCommand {
 	constructor(client: WAClient, handler: MessageHandler) {
 		super(client, handler, {
-			command: "bot",
+			command: "nezuko",
 			description: "Displays the info",
 			category: "general",
-			usage: `${client.config.prefix}bot`,
+			usage: `${client.config.prefix}nezuko`,
 			baseXp: 2000,
 		});
 	}
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const chitoge =
-			'https://telegra.ph/file/621c985f4ca375cfd7df7.mp4';
+			'https://telegra.ph/file/fa673b13abda5660d7f37.mp4';
 		return void this.client.sendMessage(
 			M.from,
 			{ url: chitoge },
@@ -27,7 +27,7 @@ export default class Command extends BaseCommand {
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `❤️ *NEZUKO* ❤️\n\n🏮 *Description: lol.*\n\n 🌟*CHITOGE BOT: https://github.com/ShineiIchijo/Chitoge*\n\n 🤍*SIESTA-BOT: https://github.com/Toshiro223/Siesta-Botto*\n\n 〽️*MARIN-BOT:https://github.com/Chey-san/Marin-Kitagawa-Bot*\n\n 🧣*YOUTSUBA-BOT:https://github.com/Whatup364/Yotsuba-Bot*\n\n`,
+				caption: `❤️ *NEZUKO* ❤️\n\n🏮 *Description: A Fully Oriented Whatsapp Bot Based on Chitoge Don't forget to give a star to the repo before fork.*\n\n 🌟*CHITOGE: https://github.com/ShineiIchijo/Chitoge*\n\n 📒*Chitoge-Guide: https://github.com/ShineiIchijo/Chitoge-Guides*\n\n ❤️*NEZUKO:https://github.com/Toshiro223/Nezuko*\n\n 📒*Nezuko-Guide:https://github.com/Toshiro223/Nezuko-Guides*\n\n`,
 			}
 		);
 	};
