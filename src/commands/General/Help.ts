eec7199d542f70d923493dd62aa7393ed2866d5d
Toshiro <91844428+Toshiro223@users.noreply.github.com>
2022-04-09 00:23:05 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -1,9 +1,9 @@
-import { MessageType, Mimetype } from '@adiwajshing/baileys'
-import { join } from 'path'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
-import { ISimplifiedMessage } from '../../typings'
+import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import request from '../../lib/request'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
@@ -12,19 +12,18 @@ export default class Command extends BaseCommand {
             description: 'Displays the help menu or shows the info of the command provided',
             category: 'general',
             usage: `${client.config.prefix}help (command_name)`,
-            dm: true,
-            aliases: ['h']
+            aliases: ['h', 'list', 'menu']
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            'https://telegra.ph/file/dc6c9b7e2c55a3d767635.mp4'
+            'https://telegra.ph/file/90c8d596818e948cc6a82.mp4'
         ]
         let well = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: well }, MessageType.video, {quoted:M.WAMessage,
             mimetype: Mimetype.gif,
-            caption: `
+            caption: `(❤️ω❤️)Konnichiwa👋 I'm ℕ𝕖𝕫𝕦𝕜𝕠^ℂ𝕙𝕒𝕟!
 
 ╭────────────┈
 │ ᴜꜱᴇʀ: *${M.sender.username}*
