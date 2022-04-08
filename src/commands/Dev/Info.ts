cbbd88cf05193ac3977bbe3c0493b270e782aefa
Toshiro <91844428+Toshiro223@users.noreply.github.com>
2022-04-09 02:02:56 +0530

    Update Info.ts

--- src/commands/Dev/Info.ts
+++ src/commands/Dev/Info.ts
@@ -31,7 +31,7 @@ export default class Command extends BaseCommand {
 		};
 		const uptime = () => formatTime(process.uptime());
 		await M.reply(
-			`*━━━❰🧧NEZUKO🧧❱━━━*\n\n🔮 *Groups: ${
+			`*━━━❰ 𝗡𝗲𝘇𝘂𝗸𝗼 ❱━━━*\n\n🚀 *Groups: ${
 		this.client.chats.all().filter(chat => chat.jid.endsWith('g.us')).length
 			}*\n\n🚦 *Uptime:${uptime()}*`
 		);
